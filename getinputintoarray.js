a41d6be9335e25db891ab898a279f5ce34d5eb25
saif2github <53149396+saif2github@users.noreply.github.com>
2021-06-28 15:34:27 +0530

    Update getinputintoarray.js

--- getinputintoarray.js
+++ getinputintoarray.js
@@ -2,7 +2,7 @@ var array = $(".uk-form-label");
 var output = "[\n";
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
-    var text = $(element).text().toLowerCase().trim().replace(" ", "_");
+    var text = $(element).text().toLowerCase().trim().replace(/ /g, "_");
     output = output + '"' + text + '",\n';
   }
   console.log(output + "]");
