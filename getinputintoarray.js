3aa5ad4a317029a77ab397f599ebf1d3a5f1ea8a
saif2github <53149396+saif2github@users.noreply.github.com>
2021-06-28 15:42:57 +0530

    Update getinputintoarray.js

--- getinputintoarray.js
+++ getinputintoarray.js
@@ -3,6 +3,7 @@ var output = "[\n";
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
     var text = $(element).text().toLowerCase().trim().replace(/ /g, "_");
+//  $(element2).next().children().attr("name",text);
     output = output + '"' + text + '",\n';
   }
   console.log(output + "]");
